2f9eac37bdc2b4c2539c5b42cb24455e5374ff28
Imagemaker <imagemaker@MacBook-Pro-de-Imagemaker.local>
2020-10-15 16:20:42 -0300

    Fin sección 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './../app-routing.module'
-import { SharedModule } from './../shared/shared.module'
-import { ComponentsModule } from './../components/components.module'
+import { AppRoutingModule } from './../app-routing.module';
+import { SharedModule } from './../shared/shared.module';
+import { ComponentsModule } from './../components/components.module';
 
-import { PagesComponent } from './pages.component'
-import { Grafica1Component } from './grafica1/grafica1.component'
-import { ProgressComponent } from './progress/progress.component'
+import { PagesComponent } from './pages.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AccountSettingComponent } from './account-setting/account-setting.component'
+import { AccountSettingComponent } from './account-setting/account-setting.component';
 
 @NgModule({
-    declarations: [DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent, AccountSettingComponent],
-    exports: [DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent],
-    imports: [CommonModule, SharedModule, AppRoutingModule, FormsModule, ComponentsModule],
+  declarations: [DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent, AccountSettingComponent],
+  exports: [DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent],
+  imports: [CommonModule, SharedModule, AppRoutingModule, FormsModule, ComponentsModule]
 })
 export class PagesModule {}
